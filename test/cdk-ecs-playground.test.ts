be074f31c756e2c663efcdb47c7fd604d779bbef
maku693 <me@shotahamada.com>
2020-02-14 17:01:17 +0000

    Initial commit
    npx aws-cdk init sample-app --language=typescript

---
+++ test/cdk-ecs-playground.test.ts
@@ -0,0 +1,21 @@
+import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import CdkEcsPlayground = require('../lib/cdk-ecs-playground-stack');
+
+test('SQS Queue Created', () => {
+    const app = new cdk.App();
+    // WHEN
+    const stack = new CdkEcsPlayground.CdkEcsPlaygroundStack(app, 'MyTestStack');
+    // THEN
+    expectCDK(stack).to(haveResource("AWS::SQS::Queue",{
+      VisibilityTimeout: 300
+    }));
+});
+
+test('SNS Topic Created', () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new CdkEcsPlayground.CdkEcsPlaygroundStack(app, 'MyTestStack');
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
+});
