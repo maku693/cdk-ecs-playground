80f35779e9a7f987e79085c5713794bac5e63818
maku693 <me@shotahamada.com>
2020-02-15 05:58:36 +0900

    npm run fix

--- bin/cdk-ecs-playground.ts
+++ bin/cdk-ecs-playground.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
-import { CdkEcsPlaygroundStack } from '../lib/cdk-ecs-playground-stack';
+import * as cdk from "@aws-cdk/core";
+import { CdkEcsPlaygroundStack } from "../lib/cdk-ecs-playground-stack";
 
 const app = new cdk.App();
-new CdkEcsPlaygroundStack(app, 'CdkEcsPlaygroundStack');
+new CdkEcsPlaygroundStack(app, "CdkEcsPlaygroundStack");
--- test/cdk-ecs-playground.test.ts
+++ test/cdk-ecs-playground.test.ts
@@ -1,21 +1,23 @@
-import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import CdkEcsPlayground = require('../lib/cdk-ecs-playground-stack');
+import { expect as expectCDK, haveResource } from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import CdkEcsPlayground = require("../lib/cdk-ecs-playground-stack");
 
-test('SQS Queue Created', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new CdkEcsPlayground.CdkEcsPlaygroundStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(haveResource("AWS::SQS::Queue",{
-      VisibilityTimeout: 300
-    }));
-});
-
-test('SNS Topic Created', () => {
+test("SQS Queue Created", () => {
   const app = new cdk.App();
   // WHEN
-  const stack = new CdkEcsPlayground.CdkEcsPlaygroundStack(app, 'MyTestStack');
+  const stack = new CdkEcsPlayground.CdkEcsPlaygroundStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(
+    haveResource("AWS::SQS::Queue", {
+      VisibilityTimeout: 300
+    })
+  );
+});
+
+test("SNS Topic Created", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new CdkEcsPlayground.CdkEcsPlaygroundStack(app, "MyTestStack");
   // THEN
   expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
 });
