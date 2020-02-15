4f37daf20104e03312b2b9e6547592a886960893
maku693 <me@shotahamada.com>
2020-02-15 19:45:22 +0900

    Add cognito authentication

--- bin/cdk-ecs-playground.ts
+++ bin/cdk-ecs-playground.ts
@@ -3,4 +3,19 @@ import * as cdk from "@aws-cdk/core";
 import { CdkEcsPlaygroundStack } from "../lib/cdk-ecs-playground-stack";
 
 const app = new cdk.App();
-new CdkEcsPlaygroundStack(app, "CdkEcsPlaygroundStack");
+
+new CdkEcsPlaygroundStack(app, "CdkEcsPlaygroundStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  },
+  cognitoUserPoolArn: app.node.tryGetContext("cognitoUserPoolArn") as string,
+  cognitoUserPoolClientId: app.node.tryGetContext(
+    "cognitoUserPoolClientId"
+  ) as string,
+  cognitoUserPoolDomain: app.node.tryGetContext(
+    "cognitoUserPoolDomain"
+  ) as string,
+  domainName: app.node.tryGetContext("domainName") as string,
+  hostedZoneDomainName: app.node.tryGetContext("hostedZoneDomainName") as string
+});
--- lib/cdk-ecs-playground-stack.ts
+++ lib/cdk-ecs-playground-stack.ts
@@ -1,24 +1,34 @@
-// import * as ec2 from "@aws-cdk/aws-ec2";
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
 import * as ecs from "@aws-cdk/aws-ecs";
 import * as ecsPatterns from "@aws-cdk/aws-ecs-patterns";
-import * as cdk from "@aws-cdk/core";
+import * as elbv2 from "@aws-cdk/aws-elasticloadbalancingv2";
+import * as route53 from "@aws-cdk/aws-route53";
+
+export interface CdkEcsPlaygroundStackProps extends cdk.StackProps {
+  cognitoUserPoolArn: string;
+  cognitoUserPoolClientId: string;
+  cognitoUserPoolDomain: string;
+  domainName: string;
+  hostedZoneDomainName: string;
+}
 
 export class CdkEcsPlaygroundStack extends cdk.Stack {
-  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props: CdkEcsPlaygroundStackProps) {
     super(scope, id, props);
 
-    // const vpc = ec2.Vpc.fromLookup(this, "VPC", { ... });
-    // const cluster = new ecs.Cluster(this, "Cluser", { vpc });
     const loadBalancedService = new ecsPatterns.ApplicationLoadBalancedFargateService(
       this,
       "Service",
       {
-        // cluster,
+        protocol: elbv2.ApplicationProtocol.HTTPS,
+        domainName: props.domainName,
+        domainZone: route53.HostedZone.fromLookup(this, "HostedZone", {
+          domainName: props.hostedZoneDomainName
+        }),
         taskImageOptions: {
           image: ecs.ContainerImage.fromRegistry("nginx:alpine")
         }
-        // publicLoadBalancer: true,
-        // certificate: ...
       }
     );
 
@@ -28,5 +38,40 @@ export class CdkEcsPlaygroundStack extends cdk.Stack {
       "deregistration_delay.timeout_seconds",
       "120"
     );
+
+    // Allow ALB to connect anyone on HTTPS for Cognito authentication
+    // cf. https://docs.aws.amazon.com/elasticloadbalancing/latest/application/listener-authenticate-users.html#configure-user-authentication
+    loadBalancedService.listener.connections.allowToAnyIpv4(
+      ec2.Port.tcp(443),
+      "Load balancer to anyone on port 443"
+    );
+
+    // Register listener rule for authentication
+    new elbv2.CfnListenerRule(this, "CognitoAuthenticationListenerRule", {
+      actions: [
+        {
+          authenticateCognitoConfig: {
+            userPoolArn: props.cognitoUserPoolArn,
+            userPoolClientId: props.cognitoUserPoolClientId,
+            userPoolDomain: props.cognitoUserPoolDomain
+          },
+          type: "authenticate-cognito",
+          order: 1
+        },
+        {
+          targetGroupArn: loadBalancedService.targetGroup.targetGroupArn,
+          type: "forward",
+          order: 2
+        }
+      ],
+      conditions: [
+        {
+          field: "path-pattern",
+          pathPatternConfig: { values: ["/*"] }
+        }
+      ],
+      listenerArn: loadBalancedService.listener.listenerArn,
+      priority: 10
+    });
   }
 }
